f04a0dba04b0bd65d2c640cac1a41062a2e54e11
Josh Manders <josh@joshmanders.com>
2023-10-03 13:20:06 -0500

    formatting

--- templates/web/config/sentry.ts
+++ templates/web/config/sentry.ts
@@ -1,5 +1,6 @@
 import Env from '@ioc:Adonis/Core/Env';
 import { sentryConfig } from '@kubit/sentry';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 
 /*
 |--------------------------------------------------------------------------
@@ -10,4 +11,10 @@ import { sentryConfig } from '@kubit/sentry';
 export default sentryConfig({
   dsn: Env.get('SENTRY_DSN'),
   environment: Env.get('NODE_ENV'),
+  // Performance Monitoring
+  integrations: [new ProfilingIntegration()],
+  enableTracing: true,
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
 });
