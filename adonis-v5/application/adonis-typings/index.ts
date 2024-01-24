a058eac92af64f3f1259c99fb5c63fcfbb6fe685
Josh Manders <josh@joshmanders.com>
2024-01-24 00:38:00 -0600

    Update package references in adonis-typings

--- adonis-v5/application/adonis-typings/index.ts
+++ adonis-v5/application/adonis-typings/index.ts
@@ -1,15 +1,15 @@
 /*
- * @adonisjs/application
+ * @kubit/application
  *
- * (c) Harminder Virk <virk@adonisjs.com>
+ * (c) Harminder Virk <virk@kubit.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-/// <reference path="@kubit/config/build/adonis-typings/config.d.ts" />
-/// <reference path="@kubit/env/build/adonis-typings/env.d.ts" />
-/// <reference path="@kubit/logger/build/adonis-typings/logger.d.ts" />
-/// <reference path="@kubit/profiler/build/adonis-typings/profiler.d.ts" />
+/// <reference path="../../../node_modules/@kubit/config/build/adonis-typings/config.d.ts" />
+/// <reference path="../../../node_modules/@kubit/env/build/adonis-typings/env.d.ts" />
+/// <reference path="../../../node_modules/@kubit/logger/build/adonis-typings/logger.d.ts" />
+/// <reference path="../../../node_modules/@kubit/profiler/build/adonis-typings/profiler.d.ts" />
 /// <reference path="./application.ts" />
 /// <reference path="./helpers.ts" />
