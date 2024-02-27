806a375bbb99799c01f8835caf92d17187a03f5d
Josh Manders <josh@joshmanders.com>
2024-02-27 10:16:52 -0600

    Copy over Mail (closes #28)

---
+++ templates/web/contracts/mail.ts
@@ -0,0 +1,14 @@
+/**
+ * Contract source: https://git.io/JvgAT
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+import { InferMailersFromConfig } from '@kubit/mail/build/config';
+
+import mailConfig from '../config/mail';
+
+declare module '@ioc:Kubit/Mail' {
+  interface MailersList extends InferMailersFromConfig<typeof mailConfig> {}
+}
