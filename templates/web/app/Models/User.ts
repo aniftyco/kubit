6e3fa0216fadaf203ccc99b12219395910fb1218
Josh Manders <josh@joshmanders.com>
2024-02-26 21:58:32 -0600

    Copy over Auth (closes #27)

---
+++ templates/web/app/Models/User.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon';
+
+import Hash from '@ioc:Kubit/Hash';
+import { BaseModel, beforeSave, column } from '@ioc:Kubit/Lucid/Orm';
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public email: string;
+
+  @column({ serializeAs: null })
+  public password: string;
+
+  @column()
+  public rememberMeToken: string | null;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password);
+    }
+  }
+}
