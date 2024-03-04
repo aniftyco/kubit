afb1d0c56f9d4c17f168def8829d290cc4ff8555
Josh Manders <josh@joshmanders.com>
2024-03-03 19:12:40 -0600

    update host/port env vars

--- templates/web/bootstrap/env.ts
+++ templates/web/bootstrap/env.ts
@@ -15,11 +15,11 @@
 import Env from '@ioc:Kubit/Env';
 
 export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
-  PORT: Env.schema.number(),
+  HOST: Env.schema.string.optional({ format: 'host' }),
+  PORT: Env.schema.number.optional(),
+  CACHE_VIEWS: Env.schema.boolean.optional(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
-  CACHE_VIEWS: Env.schema.boolean(),
   SESSION_DRIVER: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
