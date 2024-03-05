7a046eadd04b28d58b051938cf8a8e7c9ab2b59f
Josh Manders <josh@joshmanders.com>
2024-03-04 20:39:25 -0600

    Update import path for environment variable types

--- templates/web/contracts/env.ts
+++ templates/web/contracts/env.ts
@@ -11,10 +11,10 @@ declare module '@ioc:Kubit/Env' {
   | Getting types for validated environment variables
   |--------------------------------------------------------------------------
   |
-  | The `default` export from the "../env.ts" file exports types for the
-  | validated environment variables. Here we merge them with the `EnvTypes`
-  | interface so that you can enjoy intellisense when using the "Env"
-  | module.
+  | The `default` export from the "../bootstrap/env.ts" file exports types
+  | for the validated environment variables. Here we merge them with the
+  | `EnvTypes` interface so that you can enjoy intellisense when using
+  | the "Env" module.
   |
   */
 
