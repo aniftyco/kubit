9cf2d0e193be6ac6433bda746fdb4a97379b5076
Josh Manders <josh@joshmanders.com>
2025-11-09 03:03:29 -0600

    improve the types

--- packages/core/orm.d.ts
+++ packages/core/orm.d.ts
@@ -1,50 +1,47 @@
-declare module "kubit:orm" {
+declare module 'kubit:orm' {
   export class Model {
     public $attributes: Record<string, any>;
     public $dirty: Record<string, any>;
     public save(): Promise<void>;
     public destroy(): Promise<void>;
 
-    static find<T extends Model>(
-      this: { new (): T },
-      id: number
-    ): Promise<T | null>;
+    static find<T extends Model>(this: { new (): T }, id: number): Promise<T | null>;
   }
 
-  export type DecoratorFn = (target: any, property: any) => void;
-
   export const column: {
-    (options?: Record<string, unknown>): DecoratorFn;
-    dateTime(options?: Record<string, unknown>): DecoratorFn;
+    (options?: Record<string, unknown>): PropertyDecorator;
+    dateTime(options?: Record<string, unknown>): PropertyDecorator;
   };
 
-  export type Events = "save" | "create" | "update" | "destroy";
+  export type Events = 'save' | 'create' | 'update' | 'destroy';
 
   export const before: {
-    (event: Events): DecoratorFn;
+    (event: Events): PropertyDecorator;
   };
 
   export const after: {
-    (event: Events): DecoratorFn;
+    (event: Events): PropertyDecorator;
   };
 
   export const hasOne: {
-    (relatedModel: () => typeof Model): DecoratorFn;
+    (relatedModel: () => typeof Model): PropertyDecorator;
   };
 
   export type HasOne<T> = T;
 
   export const hasMany: {
-    (relatedModel: () => typeof Model): DecoratorFn;
+    (relatedModel: () => typeof Model): PropertyDecorator;
   };
 
   export type Collection<T> = T[];
 
   export type HasMany<T> = Collection<T>;
 
+  export type Trait = (model: typeof Model) => void;
+
   export const use: {
-    (plugin: (model: typeof Model) => void): DecoratorFn;
+    (...traits: Trait[]): ClassDecorator;
   };
 
-  export const SoftDeletes: (model: typeof Model) => void;
+  export const SoftDeletes: Trait;
 }
