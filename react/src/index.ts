c507978875f3ce0b0e52c4440cc3a21a2ed3a690
Josh Manders <josh@joshmanders.com>
2026-07-31 15:11:27 -0700

    add: react package entrypoint
    Co-Authored-By: Dunnbot <dunnbot@joshmanders.com>

---
+++ react/src/index.ts
@@ -0,0 +1,5 @@
+export { clsx, RADIUS_TOKENS, SPACING_TOKENS } from './clsx.js';
+export { Button } from './Button.js';
+export type { ButtonProps, ButtonSize, ButtonVariant } from './Button.js';
+export { Icon } from './Icon.js';
+export type { IconProps, IconVariant } from './Icon.js';
